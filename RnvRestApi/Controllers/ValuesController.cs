950e26a001dc88e961bf3334b60735df8a2d6096
Simon Heiss <simonheiss87@gmail.com>
2017-10-28 18:48:39 +0200

    Initial commit

---
+++ RnvRestApi/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RnvRestApi.Controllers
+{
+    [Route("location")]
+    public class LocationController : Controller
+    {
+        [HttpGet("{id}")]
+        public string Get(string id)
+        {
+            return $"id: {id} name: lol, geloc: lol1, lol2";
+        }
+
+
+        [HttpGet]
+        public string SearchStation([FromQuery] string name)
+        {
+            return name;
+        }
+
+
+        [HttpGet("{id}/to/{idTo}")]
+        public string GetRoute(string id, string idTo)
+        {
+            return $"from {id} to {idTo} Via lololol";
+        }
+    }
+}
