993f465b9baf23fbc7e563f5e845067665553586
Simon Heiss <simonheiss87@gmail.com>
2017-10-31 09:48:55 +0100

    removed ticket logic from domain, as it is not needed yet

--- Domain/GameSession.cs
+++ Domain/GameSession.cs
@@ -12,7 +12,6 @@ namespace Domain
         {
             Name = name;
             PoliceOfficers = new Collection<PoliceOfficer>();
-            GameSessionId = new GameSessionId(0);
             StartTime = DateTimeOffset.Now;
             MrX = MrX.NullValue();
         }
@@ -31,74 +30,5 @@ namespace Domain
         public DateTimeOffset StartTime { get; set; }
         public MrX MrX { get; private set; }
         public ICollection<PoliceOfficer> PoliceOfficers { get; }
-
-        public void OnMrXDriven(VehicelType type, Station stationHto, Player player)
-        {
-            //speichern der bewegungen
-        }
-
-        public void OnPoliceOfficerDriven(VehicelType type, Station stationHto, Player player)
-        {
-            //speichern der bewegungen
-            MrX.Tickets.Add(type);
-            SaveGameSession();
-        }
-
-        public void OnPlayerEmpty(VehicelType type, Player player)
-        {
-            //error im client
-        }
-
-        public void AddMrX(MrX mrX)
-        {
-            if (MrX == null)
-            {
-                MrX = mrX;
-                MrX.VehicleDrivenEvent += OnMrXDriven;
-                MrX.VehicleEmpty += OnPlayerEmpty;
-            }
-            else
-            {
-                //error im client
-            }
-            SaveGameSession();
-        }
-
-        public void RemoveMrX()
-        {
-            if (MrX != null)
-            {
-                MrX.VehicleDrivenEvent -= OnMrXDriven;
-                MrX.VehicleEmpty -= OnPlayerEmpty;
-                MrX = null;
-            }
-            SaveGameSession();
-        }
-
-        private void SaveGameSession()
-        {
-            //Todo
-        }
-
-        public void AddPoliceOfficer(PoliceOfficer policeOfficer)
-        {
-            PoliceOfficers.Add(policeOfficer);
-            policeOfficer.VehicleDrivenEvent += OnPoliceOfficerDriven;
-            policeOfficer.VehicleEmpty += OnPlayerEmpty;
-            SaveGameSession();
-        }
-
-        public void RemovePoliceOfficer(PoliceOfficer policeOfficer)
-        {
-            var officer = PoliceOfficers.SingleOrDefault(p => p.PoliceOfficerId == policeOfficer.PoliceOfficerId);
-            if (officer != null)
-            {
-                officer.VehicleDrivenEvent -= OnPoliceOfficerDriven;
-                officer.VehicleEmpty -= OnPlayerEmpty;
-
-                PoliceOfficers.Remove(officer);
-            }
-            SaveGameSession();
-        }
     }
 }
--- Domain/Player.cs
+++ Domain/Player.cs
@@ -13,27 +13,9 @@ namespace Domain
 
         public string Name { get; set; }
 
-        public event Action<VehicelType, Station, Player> VehicleDrivenEvent;
-        public event Action<VehicelType, Player> VehicleEmpty;
-
         public IEnumerable<Station> VisitedStations { get; } = new Collection<Station>();
         public IEnumerable<VehicelType> UsedVehicles { get; } = new Collection<VehicelType>();
 
         public Tickets Tickets { get; }
-
-        public void drive(VehicelType type, Station station)
-        {
-            if (Tickets.GetAmmount(type) <= 0)
-            {
-                VehicleEmpty?.Invoke(type, this);
-                // validation error
-            }
-            else
-            {
-                Tickets.Drive(type);
-                VehicleDrivenEvent?.Invoke(type, station, this);
-                //throw driven event
-            }
-        }
     }
 }
