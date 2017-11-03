93fcd507dbd29445e9052b85bcbe3b8af0d812db
Lauchi <simonheiss87@gmail.com>
2017-11-03 19:46:32 +0100

    added foundation for flattened staion list

---
+++ StationAsJsonAdapter/StationNode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Domain;
+
+namespace StationAsJsonAdapter
+{
+    public class StationNode
+    {
+        public Station Station { get; }
+        public ICollection<VehicelType> AvailableVehicles { get; }
+        public ICollection<StationNode> NextNodes { get; }
+    }
+}
